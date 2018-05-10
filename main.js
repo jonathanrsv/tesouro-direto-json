880406d2fb368e28663338e05d6594c4495595ad
Mario Diniz <mariohenriquediniz@gmail.com>
2018-05-10 17:43:07 -0300

    Setting the correct type of response

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ var extract = require('./extract_tesouro');
 
 var server = http.createServer(function(request, response) {
     extract.starter(function(data) {
-        response.write(JSON.stringify(data));
-        response.end();
+    	response.writeHead(200, {"Content-Type": "application/json"});
+        response.end(JSON.stringify(data));
     });
 });
 
 server.listen(process.env.PORT || 8000);
+
