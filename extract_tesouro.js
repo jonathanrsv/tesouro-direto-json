cf2ced4ecd95886a229f6606f4f52cf7bc437599
Mario Diniz <mariodiniz@sanebox.com>
2018-05-10 11:28:43 -0300

    Updating parsing rules

--- extract_tesouro.js
+++ extract_tesouro.js
@@ -10,7 +10,7 @@ var extract = {
 
         var options = {
             host: 'www.tesouro.fazenda.gov.br',
-            path: 'tesouro-direto-precos-e-taxas-dos-titulos'
+            path: '/tesouro-direto-precos-e-taxas-dos-titulos'
         }
 
         var request = http.request(options, function(res) {
@@ -33,38 +33,9 @@ var extract = {
 
     get_data: function(data) {
         $ = cheerio.load(data);
-        $('table tbody tr').each(function(i, elem) {
-            if ($(this).children().length != 11) return false;
-            split_data = $(this).text().split('\n');
-            extract.normalize(split_data);
-        });
+        a = $('table.tabelaPrecoseTaxas:not(".sanfonado") tbody tr.camposTesouroDireto').map((a, b) => b).map((c, d) => $(d).children('td').map((e, f) => $(f).text()));
+        this.extraction_info = a.toArray().map(b => b.toArray());
         this.callback(this.extraction_info);
-    },
-
-    sanitize: function(value) {
-        var value_new = value.replace(/^\s|( )+/g, ' ');
-        var str = value_new.replace(/^\s+/, "");
-        var str2 = str.replace("\r", "");
-        return str2;
-    },
-
-    normalize: function(info) {
-        var clean_data = {
-            titulo: this.sanitize(info[1]) + ' ' + this.sanitize(info[2]).split('/')[2],
-            nome: this.sanitize(info[1]),
-            vencimento: this.sanitize(info[2]),
-            indexador: this.sanitize(info[3]),
-            tx_compra: this.sanitize(info[4]),
-            tx_venda: this.sanitize(info[5]),
-            preco_compra: this.sanitize(info[6]),
-            preco_venda: this.sanitize(info[7]),
-            rent_30_dias: this.sanitize(info[8]),
-            rent_mes_anterior: this.sanitize(info[9]),
-            rent_anual_atual: this.sanitize(info[10]),
-            rent_12_meses: this.sanitize(info[11])
-        };
-
-        this.extraction_info.push(clean_data);
     }
 }
 
